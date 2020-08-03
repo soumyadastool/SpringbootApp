1db21725e4c68a44adff66f1629359c42a943420
tl@gmail.com <Dell@Dell-PC>
2020-08-03 01:09:42 -0400

    WH-990

--- src/main/java/com/batch/MessageRunner.java
+++ src/main/java/com/batch/MessageRunner.java
@@ -8,6 +8,7 @@ public class MessageRunner implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Welcome To SpringBoot App");
+		System.out.println("Hello");
 		
 	}
 
